9810e75363cabb8018c3c655bb9a5add1cb6cdfb
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2023-07-22 23:50:07 +0200

    Update images-api.js

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -1,19 +1,22 @@
-export function fetchImages (searchQuery, page) {
-    const USER_KEY = '33518692-16d0d1fee549af403d2d26411';
-    const PER_PAGE = 12;
-    const URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '33518692-16d0d1fee549af403d2d26411';
 
-    return (
-        fetch(
-            `${URL}?q=${searchQuery}&page=${page}&key=${USER_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`)
-        .then(response => {
-            if(response.ok) {
-                        return response.json();
-            }
-        
-            return Promise.reject(
-                new Error(`${searchQuery} not found`)
-            );
-        })
-     );
+export const fetchImages = async (imageName, page, perPage) => {
+  const searchParams = new URLSearchParams({
+    q: imageName,
+    page,
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: perPage,
+  });
+    
+    const resp = await fetch(`${BASE_URL}?${searchParams}`);
+    console.log(resp)
+    if (!resp.ok) {
+        Promise.reject(new Error('Something is wrong. Please try again!'));
+    }
+    const message = resp.json();
+    console.log(message)
+    return message;
 };
